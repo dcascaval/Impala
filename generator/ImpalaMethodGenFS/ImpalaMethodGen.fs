6527fa318636aefd5b4c3e992bac25b9783b449a
Dan Cascaval <dcascava@andrew.cmu.edu>
2019-01-16 00:49:06 -0500

    move fs methodgen rewrite into new project

--- generator/ImpalaMethodGenFS/ImpalaMethodGen.fs
+++ generator/ImpalaMethodGenFS/ImpalaMethodGen.fs
@@ -1,5 +1,4 @@
-open System.ComponentModel
-open System.Web.Configuration
+open System
 
 type Binop = ADD | SUB | MUL | DIV | MOD | EQ | GT | LT | GTE | LTE | AND | OR
 
@@ -88,32 +87,33 @@ and FmtExpr = function
   | Unop (op,s) -> sprintf "(%s %s)" (FmtUnop op) (FmtExpr s)
   | Call (name,args) -> sprintf "%s(%s)" name (FmtArgs args)
   | Method (expr,name,args) -> sprintf "%s.%s(%s)" (FmtExpr expr) name (FmtArgs args)
+  | Construct (typ,args) -> sprintf "new %s(%s)" (FmtType typ) (FmtArgs args)
   | Tern (c,i,e) -> sprintf "%s ? %s : %s" (FmtExpr c) (FmtExpr i) (FmtExpr e)
 
-let rec FmtType = function 
+and FmtType = function 
   | INT -> "int"
   | BOOL -> "bool"
   | INFER -> "var"
   | Param t -> t
-  | Array t -> FmtType t ^ "[]"
+  | Array t -> FmtType t + "[]"
   | Tuple ts -> let types = String.concat "," (List.map FmtType ts) in 
-                if (List.length ts) > 1  then "(" ^ types ^ ")" else types  
+                if (List.length ts) > 1  then "(" + types + ")" else types  
   | Composite (typ,typs) -> sprintf "%s<%s>" (FmtType typ) (String.concat "," (List.map FmtType typs))
 
 let rec FmtStmt indent stm =
-  let next = indent ^ "\t" in
+  let next = indent + "\t" in
   let fmtblk b = FmtStmt indent (Block b) in
   let formatted = 
     (match stm with
     | DeclAssign (t,v,e) -> sprintf "%s %s = %s;" (FmtType t) (FmtExpr v) (FmtExpr e)
     | Assign (v,e) -> sprintf "%s = %s;" (FmtExpr v) (FmtExpr e)
-    | Return e -> "return " ^ FmtExpr e ^ ";"
+    | Return e -> "return " + FmtExpr e + ";"
 
     | Block (b) ->
-       "{\n" ^ String.concat ("\n" ^ indent) (List.map (FmtStmt next) b) ^ "\n" ^ indent ^ "}"
+       "{\n" + String.concat ("\n" + indent) (List.map (FmtStmt next) b) + "\n" + indent + "}"
     
     | Condition c -> 
-      sprintf "if (%s) %s" (FmtExpr c.case) (fmtblk c._if) ^
+      sprintf "if (%s) %s" (FmtExpr c.case) (fmtblk c._if) +
         (match c._else with 
         | None -> ""
         | Some s -> fmtblk s)
@@ -129,17 +129,17 @@ let rec FmtStmt indent stm =
       sprintf "Parallel.For(%s,%s,%s => \n %s);"
         (FmtExpr l.start) (FmtExpr l.stop) (FmtExpr l.variable) (fmtblk l.body))
    in
-   indent ^ formatted
+   indent + formatted
 
 let FmtConstraint (param,constr) = 
   sprintf "where %s : %s" (FmtType param) (FmtType constr)
 
 let FmtFunction (func : Function) = 
-  let args = String.concat "," (List.map (fun (t,s) -> FmtType t ^ " " ^ FmtExpr s) func.args) in
+  let args = String.concat "," (List.map (fun (t,s) -> FmtType t + " " + (FmtExpr s)) func.args) in
   let sign = sprintf "%s %s\n(%s)" (FmtType func.typ) (func.name) args in
   let cons = String.concat "\n\t" (List.map FmtConstraint func.constraints) in 
   let body = FmtStmt "\t" (Block func.body) in
-  sign ^ cons ^ body
+  sign + cons + body
 
 let FmtProgram = 
   List.map (FmtFunction)
@@ -150,6 +150,24 @@ let range n =
 let maprange n f =  
   List.map f (range n)
  
+let in_param nth =  
+    let letter n = Char.ConvertFromUtf32 n in
+    Param (String.replicate (nth / 13) (letter (nth % 13)))
+
+let out_param nth = 
+    let letter n = Char.ConvertFromUtf32 n in
+    Param (String.replicate (nth / 13) (letter ((nth % 13) + 13)))
+
+let GH_Structure types =
+    Composite (Param "GH_Structure", types)
+
+let declareResults n =  
+    let result i =
+        let var = sprintf "result%d" i in
+        DeclAssign (INFER,Var var,Construct(GH_Structure [out_param i],[]))
+    in
+    range n |> List.map result
+
 
 // let red1x1 rdx out = 
 //   let s = sprintf in 
