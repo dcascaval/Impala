614e812576368df3d50026718b85f2b00afb989e
Dan Cascaval <dcascava@andrew.cmu.edu>
2019-01-19 16:02:09 -0500

    finished fs methodgens (added lambdas, tuples, skeleton)

--- generator/ImpalaMethodGenFS/ImpalaMethodGen.fs
+++ generator/ImpalaMethodGenFS/ImpalaMethodGen.fs
@@ -10,12 +10,15 @@ type Expr =
   | BConst of bool
   | Var of string
   | Binop of Binop * Expr * Expr 
-  | Unop  of Unop * Expr
-  | Tern  of Expr * Expr * Expr 
-  | Construct of CSType * Expr list
+  | Unop  of Unop * Expr 
+  | Ternary  of Expr * Expr * Expr 
+  | ExpTuple of Expr list
+  | ConstrObject of CSType * Expr list
+  | ConstrArray of CSType * Expr
   | Index of Expr * Expr
   | Property of Expr * string
   | Method of Expr * string * Expr list
+  | Lambda of Expr * Expr
   | Call  of string * Expr list
  
 and CSType = 
@@ -24,7 +27,7 @@ and CSType =
   | INFER 
   | Param of string 
   | Array of CSType 
-  | Tuple of CSType list
+  | TTuple of CSType list
   | Composite of CSType * CSType list
 
 type ForLoop = { 
@@ -79,6 +82,10 @@ let FmtUnop = function
   | NEG -> "-"
   | NOT -> "!"
 
+let rec FmtTup fn args = 
+    let tup'd = String.concat "," (List.map fn args) in 
+    if (List.length args) > 1  then "(" + tup'd + ")" else tup'd  
+
 let rec FmtArgs args = (String.concat "," (List.map FmtExpr args))
 
 and FmtExpr = function 
@@ -90,8 +97,13 @@ and FmtExpr = function
   | Unop (op,s) -> sprintf "(%s %s)" (FmtUnop op) (FmtExpr s)
   | Call (name,args) -> sprintf "%s(%s)" name (FmtArgs args)
   | Method (expr,name,args) -> sprintf "%s.%s(%s)" (FmtExpr expr) name (FmtArgs args)
-  | Construct (typ,args) -> sprintf "new %s(%s)" (FmtType typ) (FmtArgs args)
-  | Tern (c,i,e) -> sprintf "%s ? %s : %s" (FmtExpr c) (FmtExpr i) (FmtExpr e)
+  | ConstrObject (typ,args) -> sprintf "new %s(%s)" (FmtType typ) (FmtArgs args)
+  | ConstrArray (typ,arg) -> sprintf "new %s[%s]" (FmtType typ) (FmtExpr arg)
+  | ExpTuple exprs -> FmtTup FmtExpr exprs 
+  | Index (obj,idx) -> sprintf "%s[%s]" (FmtExpr obj) (FmtExpr idx)
+  | Lambda (arg,exp) -> sprintf "%s => %s" (FmtExpr arg) (FmtExpr exp)
+  | Property (obj,str) -> sprintf "%s.%s" (FmtExpr obj) str
+  | Ternary (c,i,e) -> sprintf "%s ? %s : %s" (FmtExpr c) (FmtExpr i) (FmtExpr e)
 
 and FmtType = function 
   | INT -> "int"
@@ -99,8 +111,7 @@ and FmtType = function
   | INFER -> "var"
   | Param t -> t
   | Array t -> FmtType t + "[]"
-  | Tuple ts -> let types = String.concat "," (List.map FmtType ts) in 
-                if (List.length ts) > 1  then "(" + types + ")" else types  
+  | TTuple ts -> FmtTup FmtType ts  
   | Composite (typ,typs) -> sprintf "%s<%s>" (FmtType typ) (String.concat "," (List.map FmtType typs))
 
 let rec FmtStmt indent stm =
@@ -108,6 +119,7 @@ let rec FmtStmt indent stm =
   let fmtblk b = FmtStmt indent (Block b) in
   let formatted = 
     (match stm with
+    | Simple e -> (FmtExpr e) + ";"
     | DeclAssign (t,v,e) -> sprintf "%s %s = %s;" (FmtType t) (FmtExpr v) (FmtExpr e)
     | Assign (v,e) -> sprintf "%s = %s;" (FmtExpr v) (FmtExpr e)
     | Return e -> "return " + FmtExpr e + ";"
@@ -131,6 +143,7 @@ let rec FmtStmt indent stm =
     | Parallel l -> 
       sprintf "Parallel.For(%s,%s,%s => \n %s);"
         (FmtExpr l.start) (FmtExpr l.stop) (FmtExpr l.variable) (fmtblk l.body))
+
    in
    indent + formatted
 
@@ -168,29 +181,51 @@ let GH_Structure types =
 let get_result_types out graft =
     range out |> List.map (out_param) |> List.map (fun t -> if graft then Array t else t)
    
+type ImpalaFunctionArgs = {
+    zip_types : CSType list;
+    zip_tokens : Expr list; 
+    rdx_types : CSType list;
+    rdx_tokens : Expr list;
+    action_type : CSType;
+    action_token : Expr;
+    error_type : CSType;
+    error_token : Expr;
+}
 
+// FN ARGS
 let getfnargs zip red out graft =
     let convert s offset i =
         let typestr = (in_param_string (i+offset)) in  
-        (Param typestr, (s + typestr).ToLower()) 
+        (Param typestr,Var ((s + typestr).ToLower()))
     in
-    let (zip_typ,zip_ts) = range zip |> List.map (convert "zip_"  0)    |> List.unzip   
-    let (rdx_typ,rdx_ts) = range red |> List.map (convert "redux_" zip) |> List.unzip
-    let structures = List.map (fun t -> GH_Structure [t]) (zip_typ @ rdx_typ)
-    let rdx_ls = List.map (fun t -> Composite (Param "List", [t])) rdx_typ 
+    let (zip_types,zip_tokens) = range zip |> List.map (convert "zip_"  0)    |> List.unzip   
+    let (rdx_types,rdx_tokens) = range red |> List.map (convert "redux_" zip) |> List.unzip
+    let zip_strs = List.map (fun t -> GH_Structure [t]) (zip_types)
+    let rdx_strs = List.map (fun t -> GH_Structure [t]) (rdx_types)
+    let rdx_ls = List.map (fun t -> Composite (Param "List", [t])) rdx_types
     let res_typs = get_result_types out graft in 
-    let func_t = Composite (Param "Func", zip_typ @ rdx_ls @ [Tuple (res_typs)]) 
-    let err_t  = Composite (Param "ErrorChecker", zip_typ @ rdx_ls)
+    let func_t = Composite (Param "Func", zip_types @ rdx_ls @ [TTuple (res_typs)]) 
+    let err_t  = Composite (Param "ErrorChecker", zip_types @ rdx_ls)
+    { 
+      zip_types = zip_strs;
+      zip_tokens = zip_tokens;
+      rdx_types = rdx_strs;
+      rdx_tokens = rdx_tokens;
+      action_type = func_t;
+      action_token = Var "action";
+      error_type = err_t;
+      error_token = Var "error";
+    }
 
-    []
     
 
 let declareResults n =  
     let result i =
         let var = sprintf "result%d" i in
-        (DeclAssign (INFER,Var var,Construct(GH_Structure [out_param i],[])),Var var)
+        let str_typ = GH_Structure [out_param i] in
+        (DeclAssign (INFER,Var var,ConstrObject(str_typ,[])),Var var,str_typ)
     in
-    range n |> List.map result |> List.unzip
+    range n |> List.map result |> List.unzip3
 
 let callmax name tokens =    
     [DeclAssign(INFER,Var name,Call("Max",List.map (fun token -> Property (token,"Count")) tokens))]
@@ -199,7 +234,7 @@ let getpath tokens =
     [DeclAssign(INFER,Var "paths", Call("GetPathList",tokens))]
 
 let branchzip basetoken ziptoken offset n = 
-    let bzips,zips = range n |> List.map (fun i -> let k = in_param_string (i+offset) in Var (basetoken^k),Var (ziptoken^k)) 
+    let bzips,zips = range n |> List.map (fun i -> let k = in_param_string (i+offset) in Var (basetoken + k),Var (ziptoken + k)) 
                              |> List.unzip in 
     let zipexprs = (List.map (fun z -> 
         Index (Property(z,"Branches"), 
@@ -220,6 +255,15 @@ let appendranges =
     List.mapi (fun i t -> 
         let item = (sprintf ("Item%d") i) in
         Simple(Method(t,"AppendRange",[Property(Var "compute",item);Var "targpath"])))
+  
+let appendselect sel_token path_token = 
+    List.mapi (fun i t ->
+      let item = (sprintf ("Item%d") i) in
+      Simple(Method(t,"AppendRange",[
+        Method(sel_token,"Select",[Lambda(Var "item",Property (Var "item",item))]);
+        path_token
+      ]))
+    )
 
 let allcountszero tokens = 
     let count_tokens = List.map (fun tk -> Property(tk,"Count")) tokens in
@@ -245,70 +289,92 @@ let getbranchitem =
         )
     )
        
-let getcompute tokens = []
-    
+let getcompute dest tokens res_typs graft = 
+    [DeclAssign(INFER,dest,
+        Ternary(
+           Method(Var "error","Validate",[ExpTuple tokens]),
+           Call("action",tokens),
+           (if graft then
+                ExpTuple (List.map (fun t -> ConstrArray(t,IConst 0)) res_typs)
+           else 
+                Var "default")
+        )
+    )]
+
 
 let generateFunction zip redx out graft =   
-    
-
-    let (declresult_stmts,declresult_tokens) = declareResults (out + graft) in 
-    let (branchzip_stmts,branchzip_tokens) = branchzip "branchzip" "zip" 0 zip in
-    let (rdxzip_stmts,rdxzip_tokens) = branchzip "rxbranchredux" "redux" zip redx in 
+    let args = getfnargs zip redx out graft in
+    let (declresult_stms,result_toks,result_typs) = declareResults out in 
+    let (declbranchzip_stms,branchzip_tokens) = branchzip "branchzip" "zip" 0 zip in
+    let (declrdxzip_stms,rdxzip_tokens) = branchzip "rxbranchredux" "redux" zip redx in 
     
     List.concat [
-        declresult_stmts;
-        callmax "maxbranch" declresult_tokens; // Wrong tokens use args here
-        getpath declresult_tokens;
+        declresult_stms;
+        callmax "maxbranch" (args.zip_tokens @ args.rdx_tokens); 
+        getpath result_toks;
 
         [Loop { 
             variable = Var "i";
             start = IConst 0; stop = Var "maxbranch"; stride = IConst 1;
             body = List.concat [
                 [DeclAssign (INFER,Var "path",Call("GetPath",[Var "paths";Var "i"]))];
-                branchzip_stmts;
+                declbranchzip_stms;
                 callmax "maxlen" branchzip_tokens;
                 
                 [Loop {
-                  variable = Var "j";
-                  start = IConst 0; stop = Var "maxlen"; stride = IConst 1;
-                  body = 
-                    DeclAssign(INFER,Var "targpath",
-                               Method(Var "path","AppendElement",[Var "j"])) :: 
-                    ensurepaths declresult_tokens 
+                    variable = Var "j";
+                    start = IConst 0; stop = Var "maxlen"; stride = IConst 1;
+                    body = DeclAssign(INFER,Var "targpath",
+                            Method(Var "path","AppendElement",[Var "j"])) :: 
+                            ensurepaths result_toks 
                 }]
             ]
-        }]
+        }];
 
         [Parallel {
             variable = Var "i";
             start = IConst 0; stop = Var "maxbranch"; stride = IConst 1;
             body = List.concat [
                 [DeclAssign (INFER,Var "path",Call("GetPath",[Var "paths";Var "i"]))];
-                branchzip_stmts;
-                rdxzip_stmts;
+                declbranchzip_stms;
+                declrdxzip_stms;
                 
                 [Condition {
-                  case = allcountszero (branchzip_tokens @ rdxzip_tokens);  
-                  _if = List.concat [
-                    callmax "maxlen" branchzip_tokens;
-                    
-                    [Parallel { 
-                        variable = Var "j";
-                        start = IConst 0; stop = Var "maxlen"; stride = IConst 1;
-                        body = List.concat [
-                            getbranchitem branchzip_tokens;
-
-                            [DeclAssign (INFER,Var "path",Call("GetPath",[Var "paths";Var "i"]))];
-                            appendranges declresult_tokens;
-                        ];
-                    }]
-
-
-
-                  ]; 
-                  _else = None;
+                    case = allcountszero (branchzip_tokens @ rdxzip_tokens);  
+                    _if = List.concat [
+                        callmax "maxlen" branchzip_tokens;
+                         
+                        (if graft then
+                            [Parallel { 
+                                variable = Var "j";
+                                start = IConst 0; stop = Var "maxlen"; stride = IConst 1;
+                                body = List.concat [
+                                    getbranchitem args.zip_tokens branchzip_tokens;
+                                    getcompute (Var "compute") (args.zip_tokens @ args.rdx_tokens) (result_typs) graft;
+                                    [DeclAssign (INFER,Var "path",Call("GetPath",[Var "paths";Var "i"]))];
+                                    appendranges result_toks;
+                                ];
+                            }] 
+                        else
+                            List.concat [
+                                [DeclAssign (INFER,Var "temp",ConstrArray(TTuple(result_typs),Var"maxlen"));
+                                 Parallel {
+                                    variable = Var "j"; 
+                                    start = IConst 0; stop = Var "maxlen"; stride = IConst 1;
+                                    body = List.concat [
+                                        getbranchitem args.zip_tokens branchzip_tokens;
+                                        getcompute (Index(Var "temp",Var "j")) (args.zip_tokens @ args.rdx_tokens) (result_typs) graft;
+                                    ]
+                                 }]; 
+                                appendselect (Var "temp") (Var "path") result_toks;
+                            ]
+                        )
+                    ]; 
+                    _else = None;
                 }]
 
-                ]
-        }]
+            ]
+        }];
+
+        [Return (ExpTuple(result_toks))]
     ]
