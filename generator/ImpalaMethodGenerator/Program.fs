5c5cef8cb159d7a5b7dbd084f82071bba686616e
Dan Cascaval <dcascava@andrew.cmu.edu>
2019-01-21 16:20:20 -0500

    fully replace methodgen with fs version!

--- generator/ImpalaMethodGenerator/Program.fs
+++ generator/ImpalaMethodGenerator/Program.fs
@@ -1,18 +1,13 @@
 ﻿
 module Main
 
-open System
 open Generator
 open Bindings
 open System.IO
 
-// Todo: 
-// Fix 0-item branchy boiis 
-
     [<EntryPoint>]
     let main argv = 
-        let file = "C:\\Users\\Dan Cascaval\\Desktop\\Repos\\Impala\\Impala\\TestGeneration.cs"
+        let file = ".\\..\\..\\..\\..\\Impala\\Generated.cs"
         let _ = File.WriteAllText(file,"")                // Out with the old 
         let _ = PrintProgram file (GenerateProgram 7 4 4) // In With the  new
-        //Console.ReadLine() |> ignore                    // Block for debugger
         0 
